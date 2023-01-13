5a5a89743b01e38aa2502741f4a387813162742f
demiahmed <demi.ahmed@gmail.com>
2023-01-13 12:42:44 +0800

    fixed student title and dynamic contacts

--- js/studentAutoFill.js
+++ js/studentAutoFill.js
@@ -1,10 +1,11 @@
 d3.csv(`../../../assets/drivers/2022.csv`, function(data){
-    // data.fil
-    // console.log(data);
+
+    try {
+        
+
     let studentName = window.location.href.split("/").at(-2)
-    // console.log(String(studentName));
     studentData = data.filter(e => e.key==String(studentName))[0]
-    console.log(studentData);
+    // console.log(studentData);
 
     // $("body").append(``)
     $("body").append(`<title-text title="${studentData.title}"></title-text>`)
@@ -25,7 +26,9 @@ d3.csv(`../../../assets/drivers/2022.csv`, function(data){
     linkedin="${studentData.linkedin}"
      ></project-bio>`)
 
-     $(".titletext").css("padding-top", "10vw")
+     $(".titletext").css("padding-top", "16vh")
+     $(".titletext").css("font-weight", "bold")
+     $(".titletext").css("font-size", "3rem")
      $(".titletext").css("margin-top", "0")
      $(".abstract").css("padding-bottom", "4vh")
 
@@ -33,20 +36,27 @@ d3.csv(`../../../assets/drivers/2022.csv`, function(data){
 
     for (let index = 1; index < 20; index++) {
         $("body").append(`<special-image url="./data/${index}.jpg"></special-image>`)
-
-        
     }
     //  <special-image url="./data/Untitled-1.jpg"></special-image>
+    JSON.parse(studentData.video).map(e => $("body").append(`<video-container url=${e}></video-container>`))
+
+    } catch (error) {
+        console.log(error)
+    }
+
     $("body").append(`<div class="bottomlinks">
     <h3><a  href="javascript:window.scrollTo(0,0)">Back to Top</a></h3>
-</div>`)
+    </div>`)
+
     $("body").append(`<footer-i></footer-i>`)
 
 
     // Removing icons which aren't needed
     let icons = $(".contact > a")
-    elemsToRemove = [studentData.email, studentData.issuu, studentData.website, studentData.instagram, studentData.linkedin].map((e,i) => e=="0"? i:undefined).filter(x => x)
-    elemsToRemove.map(e => icons[e].remove())
+
+    elemsToRemove = [studentData.email, studentData.issuu, studentData.website, studentData.instagram, studentData.linkedin].map((e,i) =>  e=="0"? i:"ig")
+    // console.log(elemsToRemove);
+    elemsToRemove.map(e => { if(e!=="ig") icons[e].remove()})
 
 })
 
