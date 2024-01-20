09ad6ee3d2724d17c04470e46e7d588a2f7a4a6c
demiahmed <40930082+demiahmed@users.noreply.github.com>
2024-01-20 13:43:58 +0800

    content refresh rush

--- js/filter-gallery.js
+++ js/filter-gallery.js
@@ -1,147 +1,158 @@
-d3.csv(`${document.querySelector("#router").getAttribute('filepath')}`, function(data){
+d3.csv(
+  `${document.querySelector("#router").getAttribute("filepath")}`,
+  function (data) {
     console.log(data);
-var PHOTODATA = [];
-let processedName;
+    var PHOTODATA = [];
+    let processedName;
 
-for(var i=0;i<data.length;i++) {
-    processedName = data[i].key
-    var photo = {
+    for (var i = 0; i < data.length; i++) {
+      processedName = data[i].key;
+      var photo = {
         id: i,
         category: data[i].Topic,
         name: data[i].name,
-        url: `${document.querySelector('#router').getAttribute('prefix')}../projects/${document.querySelector("#router").getAttribute('year')}/${processedName}/`,
+        url: `${document
+          .querySelector("#router")
+          .getAttribute("prefix")}../projects/${document
+          .querySelector("#router")
+          .getAttribute("year")}/${processedName}/`,
         //iu: "./explore/" + String(data[i].name) ,
-        path: `${document.querySelector('#router').getAttribute('prefix')}../projects/${document.querySelector("#router").getAttribute('year')}/${processedName}/${processedName}`
-    };
-    // console.log(photo.url, photo.path);
-    PHOTODATA.push(photo);
+        path: `${document
+          .querySelector("#router")
+          .getAttribute("prefix")}../projects/${document
+          .querySelector("#router")
+          .getAttribute("year")}/${processedName}/${processedName}`,
+      };
+      // console.log(photo.url, photo.path);
+      PHOTODATA.push(photo);
+    }
 
-}
+    // console.log(PHOTODATA);
 
-// console.log(PHOTODATA);
+    {
+      /*Create a component of the filter panel*/
+    }
 
+    var FilterPanel = React.createClass({
+      render: function () {
+        return (
+          <button className="button" onClick={this.props.onClick}>
+            {this.props.category}
+          </button>
+        );
+      },
+    });
 
+    {
+      /*Create a layout component for one photo*/
+    }
 
-{/*Create a component of the filter panel*/}
+    var Photo = React.createClass({
+      render: function () {
+        return (
+          <a className="photo-container" href={String(this.props.url)}>
+            <div className="wrapper" data-category={this.props.category}>
+              <img
+                className="back-image"
+                src={this.props.path + ".jpg"}
+                onError={(e) =>
+                  (e.target.src = this.props.path + ".png")
+                  (e.target.src = "../../assets/fallbackimgs/missing.jpg")
+                }
+              />
+              <img
+                className="front-image"
+                src={this.props.path + "-project.jpg"}
+                onError={(e) => {
+                  e.target.src = this.props.path + "-project.png"; // Try loading PNG if JPG fails
+                  e.target.onerror = null; // Remove the fallback for PNG if it also fails
+                }}
+              />{" "}
+              {/* <img className = "front-image" src="./assets/awards/zaha.jpg" /> */}
+              <div className="overlay">
+                <div className="content">{this.props.name}</div>
+              </div>
+            </div>
+          </a>
+        );
+      },
+    });
 
-var FilterPanel = React.createClass({
-
-render: function(){
-return (
-    <button className = "button" onClick={this.props.onClick}>{this.props.category}</button>
-
-)
-}
-
-});
-
-
-{/*Create a layout component for one photo*/}
-
-var Photo =  React.createClass({
-
-render: function(){
-return (
-    <a className = "photo-container" href= {String(this.props.url)} >
-        <div className= "wrapper" data-category={this.props.category} >
-            
-
-                <img className = "back-image" src={this.props.path + ".jpg"} onError={(e) => ((e.target.src = "../../assets/fallbackimgs/missing.jpg"))} />
-                <img className = "front-image" src={this.props.path + "-project.jpg"} onError={(e) => ((e.target.src = "../../assets/fallbackimgs/missing.jpg"))} />
-                {/* <img className = "front-image" src="./assets/awards/zaha.jpg" /> */}
-                <div className= "overlay">
-                    <div className="content">{this.props.name}
-                    </div>
-                </div>
-        </div>
-        </a>
-       );
-}
-
-
-});
-
-
-
-{/*Create a final collection of photos in photoGallery*/}
-var PhotoGallery = React.createClass({
-
-getInitialState: function() {
+    {
+      /*Create a final collection of photos in photoGallery*/
+    }
+    var PhotoGallery = React.createClass({
+      getInitialState: function () {
         return {
-            displayedCategories: PHOTODATA,
-            active: false
+          displayedCategories: PHOTODATA,
+          active: false,
         };
-    },
-toggleActive: function() {
-this.setState({
-    active: !this.state.active
-});
-},
+      },
+      toggleActive: function () {
+        this.setState({
+          active: !this.state.active,
+        });
+      },
 
-selectCategory: function(element){
-// console.log('element is: '+ element);
-  var categoryName = element.toLowerCase();
-        var displayedCategories = PHOTODATA.filter(function(el) {
-            
-            var searchValue = el.category.toLowerCase();
-            return searchValue.indexOf(categoryName) !== -1;
+      selectCategory: function (element) {
+        // console.log('element is: '+ element);
+        var categoryName = element.toLowerCase();
+        var displayedCategories = PHOTODATA.filter(function (el) {
+          var searchValue = el.category.toLowerCase();
+          return searchValue.indexOf(categoryName) !== -1;
         });
 
         this.setState({
-            displayedCategories: displayedCategories
+          displayedCategories: displayedCategories,
+        });
+      },
+      resetFilter: function (element) {
+        this.setState({
+          displayedCategories: PHOTODATA,
+        });
+      },
 
+      render: function () {
+        var buttonClass = this.state.active ? "active" : "";
+        var categoryToSelect = this.selectCategory;
+        var getUniqueCategories = [];
+        PHOTODATA.forEach(function (el) {
+          if (getUniqueCategories.indexOf(el.category) === -1)
+            getUniqueCategories.push(el.category);
         });
 
-},
-resetFilter: function(element){
-this.setState({
-            displayedCategories: PHOTODATA
+        return (
+          <div className="overlay-photogallery">
+            <div className="filter-panel">
+              {getUniqueCategories.map(function (el, i) {
+                var boundClick = categoryToSelect.bind(null, el);
+                return (
+                  <FilterPanel onClick={boundClick} category={el} key={i} />
+                );
+              })}
+              <button className="resetBtn" onClick={this.resetFilter}>
+                {" "}
+                Reset Filter{" "}
+              </button>
+            </div>
+            <div className="PhotoGallery">
+              {this.state.displayedCategories.map(function (el) {
+                return (
+                  <Photo
+                    key={el.id}
+                    url={el.url}
+                    category={el.category}
+                    path={el.path}
+                    name={el.name}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        );
+      },
+    });
 
-        });
-},
-
-
-
-render: function(){
-var buttonClass = this.state.active ? 'active' : '';
-var categoryToSelect = this.selectCategory;
-var getUniqueCategories=[];
-PHOTODATA.forEach(function(el){
-    if(getUniqueCategories.indexOf(el.category) === -1 ) getUniqueCategories.push(el.category);
-})
-
-return (
-    <div className="overlay-photogallery">
-        <div className="filter-panel">
-            {
-                getUniqueCategories.map(function(el,i){
-                    var boundClick = categoryToSelect.bind(null,el);
-                    return <FilterPanel onClick={boundClick} category={el} key={i} />
-                })
-
-            }		
-            <button className="resetBtn" onClick={this.resetFilter}> Reset Filter </button>
-        </div>
-        <div className="PhotoGallery">
-            {
-
-                this.state.displayedCategories.map(function(el){
-                            return <Photo key={el.id} url={el.url} category={el.category} path={el.path} name={el.name} />
-                        })
-
-            }
-        </div>
-    </div>
-    );
-
-
-}
-
-
-
-});
-
-ReactDOM.render(<PhotoGallery />, document.getElementById('main'));
-
-});
-
+    ReactDOM.render(<PhotoGallery />, document.getElementById("main"));
+  }
+);
--- js/studentAutoFill.js
+++ js/studentAutoFill.js
@@ -1,14 +1,11 @@
-d3.csv(`../../../assets/drivers/2022.csv`, function(data){
-
-    try {
-        
-
-    let studentName = window.location.href.split("/").at(-2)
-    studentData = data.filter(e => e.key==String(studentName))[0]
-    // console.log(studentData);
+d3.csv(`../../../assets/drivers/2023.csv`, function (data) {
+  try {
+    let studentName = window.location.href.split("/").at(-2);
+    // console.log(data);
+    studentData = data.filter((e) => e.key == String(studentName))[0];
 
     // $("body").append(``)
-    $("body").append(`<title-text title="${studentData.title}"></title-text>`)
+    $("body").append(`<title-text title="${studentData.title}"></title-text>`);
     $("body").append(`<project-bio 
     main-title="Abstract"
     summary="${studentData.summary}"
@@ -24,39 +21,51 @@ d3.csv(`../../../assets/drivers/2022.csv`, function(data){
     website="${studentData.website}"
     instagram="${studentData.instagram}"
     linkedin="${studentData.linkedin}"
-     ></project-bio>`)
+     ></project-bio>`);
 
-     $(".titletext").css("padding-top", "16vh")
-     $(".titletext").css("font-weight", "bold")
-     $(".titletext").css("font-size", "3rem")
-     $(".titletext").css("margin-top", "0")
-     $(".abstract").css("padding-bottom", "4vh")
-
-
+    $(".titletext").css("padding-top", "16vh");
+    $(".titletext").css("font-weight", "bold");
+    $(".titletext").css("font-size", "3rem");
+    $(".titletext").css("margin-top", "0");
+    $(".abstract").css("padding-bottom", "4vh");
 
     for (let index = 1; index < 20; index++) {
-        $("body").append(`<special-image url="./data/${index}.jpg"></special-image>`)
+      // Use template literals to dynamically generate the URL with both JPG and PNG formats
+      const imageUrlJpg = `./data/${index}.jpg`;
+      const imageUrlJPG = `./data/${index}.JPG`;
+      const imageUrlPng = `./data/${index}.png`;
+
+      // Append the image elements with both JPG and PNG formats
+      $("body").append(`<special-image url="${imageUrlJpg}"></special-image>`);
+    //   $("body").append(`<special-image url="${imageUrlJPG}"></special-image>`);
+      $("body").append(`<special-image url="${imageUrlPng}"></special-image>`);
     }
     //  <special-image url="./data/Untitled-1.jpg"></special-image>
-    JSON.parse(studentData.video).map(e => $("body").append(`<video-container url=${e}></video-container>`))
+    JSON.parse(studentData.video).map((e) =>
+      $("body").append(`<video-container url=${e}></video-container>`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 
-    } catch (error) {
-        console.log(error)
-    }
-
-    $("body").append(`<div class="bottomlinks">
+  $("body").append(`<div class="bottomlinks">
     <h3><a  href="javascript:window.scrollTo(0,0)">Back to Top</a></h3>
-    </div>`)
+    </div>`);
 
-    $("body").append(`<footer-i></footer-i>`)
+  $("body").append(`<footer-i></footer-i>`);
 
+  // Removing icons which aren't needed
+  let icons = $(".contact > a");
 
-    // Removing icons which aren't needed
-    let icons = $(".contact > a")
-
-    elemsToRemove = [studentData.email, studentData.issuu, studentData.website, studentData.instagram, studentData.linkedin].map((e,i) =>  e=="0"? i:"ig")
-    // console.log(elemsToRemove);
-    elemsToRemove.map(e => { if(e!=="ig") icons[e].remove()})
-
-})
-
+  elemsToRemove = [
+    studentData.email,
+    studentData.issuu,
+    studentData.website,
+    studentData.instagram,
+    studentData.linkedin,
+  ].map((e, i) => (e == "0" ? i : "ig"));
+  // console.log(elemsToRemove);
+  elemsToRemove.map((e) => {
+    if (e !== "ig") icons[e].remove();
+  });
+});
